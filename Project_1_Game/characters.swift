1368889d6bb40733ad2b310b9f89081206f06347
cgmuskat <c_garcia_muskat@hotmail.com>
2018-06-08 11:15:50 +0200

    func CharacterChoice replaces playerChoice with variables created for each Character

--- Project_1_Game/characters.swift
+++ Project_1_Game/characters.swift
@@ -1,61 +1,42 @@
 //Definition of the global character class
 class Character   {
     var characterName:String
-    var characterType: String
-    var characterWeapon: String
     var healthBar:Int
-    init(characterName: String, characterType:String, characterWeapon:String, healthBar:Int){
+    var healthDamages:Int
+    init(characterName: String, healthBar:Int,healthDamages:Int){
         self.characterName = characterName
-        self.characterType = characterType
-        self.characterWeapon = characterWeapon
         self.healthBar = healthBar
+        self.healthDamages = healthDamages
     }
-    func healthDamages() {
-        if healthBar <= 100 || healthBar >= 10{
-            healthBar -= 10;
-            print ("\(characterWeapon)Attack !\(healthDamages())")
-        }
-        else if healthBar == 10 {
-            print("You're Dead !")
-        } else {
-            print("You're already dead")
-        }
-    }
-    func healthCare(){
-        if healthBar <= 90 || healthBar >= 10{
-            healthBar += 10}
-        else {
-            print("You're already dead")
-        }
-    }
+    
 
     var description: String{
-        return "\(characterName),\(characterType),\(characterWeapon),\(healthBar)"
+        return "\(characterName),\(healthBar)"
     }
 }
 //Definition of the fighter character
 class Fighter: Character {
-    override var characterType:String {get { return "Fighter"} set {} }
-    override var characterWeapon:String {get { return "Sword"} set {} }
+    init(name: String){
+        super.init(characterName: name, healthBar: 100, healthDamages: 10)
+    }
 }
 //Definition of the Wizard character
 class Wizard: Character {
-    override var characterType:String {get { return "Wizard"} set {} }
-    override var characterWeapon:String {get { return "Dagger"} set {} }
-    override func healthCare() { super.healthBar += 30}
+    init(name: String){
+        super.init(characterName: name, healthBar: 80, healthDamages: 15)
+    }
 }
 //Definition of the Colossus character
 class Colossus: Character {
-    override var characterType:String {get { return "Colossus"} set {} }
-    override var characterWeapon:String {get { return "Mass"} set {} }
-    override func healthDamages() { super.healthBar -= 5 }
+    init(name: String){
+        super.init(characterName: name, healthBar: 140, healthDamages: 5)
+    }
 }
 //Definition of the Dwarf character
 class Dwarf: Character {
-    override var characterType:String {get { return "Dwarf"} set {} }
-    override var characterWeapon:String {get { return "Axe"} set {} }
-    override var healthBar:Int {willSet{healthBar = 100}didSet{healthBar = 80}}
-    override func healthDamages() { super.healthBar -= 30 }
+    init(name: String){
+        super.init(characterName: name, healthBar: 80, healthDamages: 20)
+    }
 }
 
 
--- Project_1_Game/teams.swift
+++ Project_1_Game/teams.swift
@@ -1,10 +1,8 @@
 import Foundation
 
 class Teams: Character {
-    /*var tableTeam1 : ["Name" :" \(characterName)","Type" :" \(characterType),,"Weapon" : "\(characterWeapon)","Life" :" \(healthBar)"]*/
-    var descriptionTeam1:String{
-        return "Name : \(characterName)\n\nType : \(characterType)\nWeapon : \(characterWeapon)\nLife : \(healthBar)"
-    }
+
+
 }
 
 

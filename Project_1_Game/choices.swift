c09703dc410ae3a55645d94787beb7987804a6f9
cgmuskat <c_garcia_muskat@hotmail.com>
2018-06-14 16:15:32 +0200

    Cleaning and making the code more readable/semantic

--- Project_1_Game/choices.swift
+++ Project_1_Game/choices.swift
@@ -1,6 +1,6 @@
 import Foundation
-//Welcome message and explication of the differents characters game's type to choice
 class Service {
+    //Empty dictionnary created
      var team = [String:Character]()
     //Explication of the differents characters game's type to choice
     func characterChoice(){
@@ -10,53 +10,9 @@ class Service {
             + "\n3. Colosse : Imposant et très résistant, mais il ne vous fera pas bien mal"
             + "\n4. Nain : Sa hache vous infligera beaucoup de dégâts, mais il n'a pas beaucoup de points de vie.\n")
     }
-    //Function witch will create the team
-    func creatingTeam(){
-         //Welcome message and rules for choices
-        print("\n\nBonjour et bienvenue dans un univers, sans règles !\nChoisis bien tes joueurs, tu dois en choisir 3 maximum! ;)\n\nQuel sera le premier personnage qui va faire parti de ton équipe ?")
-        //Choice of the first character type for the team
-        characterChoice()
-        //Insert the first choice into the table "Team"
-        team["Character1"] = playerChoice()
-        //Message confirming that the player was inserted on the table and propose the 2nd choice
-        print("\nBien !\n\nQuel sera le second personnage qui va faire parti de ton équipe ?")
-        //Choice of the second character type for the team
-        characterChoice()
-        //Insert the second choice into the table "Team"
-        team["Character2"] = playerChoice()
-        //Message confirming that the player was inserted on the table and propose the 3rd choice
-        print("\nParfait !\n\nPlus qu'un combattant à choisir !")
-        //Choice of the third character type for the team
-        characterChoice()
-        //Insert the third choice into the table "Team"
-        team["Character3"] = playerChoice()
-        func isNameValid(team:[String:Character],name: String) -> Bool{
-            for (characterName,name) in team {
-                if "\(characterName,name)" == "\(name)" {
-                    print("Vous avez déjà choisi \(name), veuillez recommencer" )
-                    creatingTeam()
-                    return false
-                }
-            }
-            print("\nVotre équipe est prête !!!\n\(team)")
-            return true
-        }
-        
-        //Checking if the same name was selected 2 times
-        /*func isNameValid(team:[String:Character], name: String) -> Bool {
-            for (characterName: name) in team {
-                if characterName == name {
-                    print("vous avez déjà choisi \(name)!\(playerChoice())")
-                    return false
-                }
-            }
-           )
-        }*/
-
-    }
-    
     //Choice of the player
     func playerChoice() -> Character{
+        characterChoice()
         if let characterChoice = readLine() {
             switch characterChoice {
             case "1":
@@ -73,27 +29,27 @@ class Service {
                     let aWizard = Wizard(name: name)
                     return aWizard
                 }   else {
-                return playerChoice()
+                    return playerChoice()
                 }
             case "3":
                 print("\nComment s'appellera votre colosse !")
                 if let name = readLine(){
                     let aColossus = Colossus(name: name)
                     return aColossus
-            } else {
-                return playerChoice()
-            }
+                } else {
+                    return playerChoice()
+                }
             case "4":
                 print("\nComment s'appellera votre nain !")
                 if let name = readLine(){
                     let aDwarf = Dwarf(name: name)
                     return aDwarf
                 }   else {
-                return playerChoice()
-            }
+                    return playerChoice()
+                }
             default:
-            print("Choisis entre 1 et 4")
-            return playerChoice()
+                print("Choisis entre 1 et 4")
+                return playerChoice()
             }
         }
         else {
@@ -101,5 +57,31 @@ class Service {
         }
     }
 
+    //Function witch will create the team
+    func creatingTeam(){
+         //Welcome message and rules for choices
+        print("\n\nBonjour et bienvenue dans un univers, sans règles !\nChoisis bien tes joueurs, tu dois en choisir 3 maximum! ;)\n\nQuel sera le premier personnage qui va faire parti de ton équipe ?")
+        //Insert the first choice into the table "Team"
+        team["Character1"] = playerChoice()
+        //Message confirming that the player was inserted on the table and propose the 2nd choice
+        print("\nBien !\n\nQuel sera le second personnage qui va faire parti de ton équipe ?")
+        //Insert the second choice into the table "Team"
+        team["Character2"] = playerChoice()
+        //Message confirming that the player was inserted on the table and propose the 3rd choice
+        print("\nParfait !\n\nPlus qu'un combattant à choisir !")
+        //Insert the third choice into the table "Team"
+        team["Character3"] = playerChoice()
+        func isNameValid(team:[String:Character],name: String) -> Bool{
+            for (characterName,name) in team {
+                if "\(characterName,name)" == "\(name)" {
+                    print("Vous avez déjà choisi \(name), veuillez recommencer" )
+                    creatingTeam()
+                    return false
+                }
+            }
+            return true
+        }
+        print("\nVotre équipe est prête !!!\n\(team)")
+    }
 }
 

b2eaac583795af53835a90bcbf8f296ebaf18f9a
lefreepatate <36165679+lefreepatate@users.noreply.github.com>
2018-07-17 17:55:56 +0200

    GitHub ^M bug

--- Project_1_Game/main.swift
+++ Project_1_Game/main.swift
@@ -1,1 +1,5 @@
-import Foundationvar firstPlayer = Player(name: "", team: [Character]())var secondPlayer = Player(name: "", team: [Character]())var game = Choices(firstPlayer: firstPlayer, secondPlayer: secondPlayer)game.game()
+import Foundation
+var firstPlayer = Player(name: "", team: [Character]())
+var secondPlayer = Player(name: "", team: [Character]())
+var game = Choices(firstPlayer: firstPlayer, secondPlayer: secondPlayer)
+game.game() 
